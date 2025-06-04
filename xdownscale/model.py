eabf23fa24044d1a4a5d3b0067152d1b0a4c1471
Manmeet Singh <manmeet20singh11@gmail.com>
2025-06-03 19:42:34 -0500

    Update model.py

--- xdownscale/model.py
+++ xdownscale/model.py
@@ -1,1 +1,16 @@
+import torch.nn as nn
+
+class SRCNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Conv2d(1, 64, kernel_size=9, padding=4),
+            nn.ReLU(),
+            nn.Conv2d(64, 32, kernel_size=1),
+            nn.ReLU(),
+            nn.Conv2d(32, 1, kernel_size=5, padding=2)
+        )
+
+    def forward(self, x):
+        return self.model(x)
 
