0c7047fee94617863ff7394ac55d311cd3dbeac6
Manmeet Singh <manmeet20singh11@gmail.com>
2025-06-03 20:07:54 -0500

    Update model.py

--- xdownscale/model.py
+++ xdownscale/model.py
@@ -14,3 +14,36 @@ class SRCNN(nn.Module):
     def forward(self, x):
         return self.model(x)
 
+class FSRCNN(nn.Module):
+    def __init__(self, d=56, s=12, m=4, upscale_factor=1):
+        super(FSRCNN, self).__init__()
+        self.first_part = nn.Sequential(
+            nn.Conv2d(1, d, kernel_size=5, padding=5//2),
+            nn.PReLU(d)
+        )
+
+        self.mid_parts = [nn.Sequential(
+            nn.Conv2d(d, s, kernel_size=1),
+            nn.PReLU(s)
+        )]
+
+        for _ in range(m - 1):
+            self.mid_parts.append(nn.Sequential(
+                nn.Conv2d(s, s, kernel_size=3, padding=3//2),
+                nn.PReLU(s)
+            ))
+
+        self.mid_parts = nn.Sequential(*self.mid_parts)
+
+        self.last_part = nn.Sequential(
+            nn.Conv2d(s, d, kernel_size=1),
+            nn.PReLU(d),
+            nn.ConvTranspose2d(d, 1, kernel_size=9, stride=upscale_factor,
+                               padding=9//2, output_padding=upscale_factor - 1)
+        )
+
+    def forward(self, x):
+        x = self.first_part(x)
+        x = self.mid_parts(x)
+        x = self.last_part(x)
+        return x
