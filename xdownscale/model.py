1c173c200ce69fdff699e4dfdae624163bb4296e
Manmeet Singh <manmeet20singh11@gmail.com>
2025-06-05 22:01:43 -0500

    Update model.py

--- xdownscale/model.py
+++ xdownscale/model.py
@@ -616,3 +616,31 @@ class DPMN(nn.Module):
         x = torch.add(x, residual)
 
         return x
+
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class SAFMN(nn.Module):
+    def __init__(self, in_channels=1, upscale_factor=1):
+        super(SAFMN, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_channels, 64, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+        self.conv4 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+        self.conv5 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+        self.conv6 = nn.Conv2d(64, in_channels * upscale_factor ** 2, kernel_size=3, padding=1)
+        self.pixel_shuffle = nn.PixelShuffle(upscale_factor)
+
+    def forward(self, x):
+        x1 = F.relu(self.conv1(x))
+        x2 = F.relu(self.conv2(x1))
+        x3 = F.relu(self.conv3(x2))
+        x4 = F.relu(self.conv4(x3))
+        x5 = F.relu(self.conv5(x4))
+        x6 = self.conv6(x5)
+        out = self.pixel_shuffle(x6)
+
+        return out
