a3100dac1b169daab629a3ceb6843bfb4ddf4b62
Manmeet Singh <manmeet20singh11@gmail.com>
2025-06-05 21:26:56 -0500

    Update model.py

--- xdownscale/model.py
+++ xdownscale/model.py
@@ -448,3 +448,69 @@ class RCAN(nn.Module):
 
         x3 = self.output_conv(x2)
         return x + x3
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# ---------------- UNet ----------------
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(DoubleConv, self).__init__()
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+
+class UNet(nn.Module):
+    def __init__(self, in_channels=1, out_channels=1, features=[64, 128, 256, 512]):
+        super(UNet, self).__init__()
+        
+        self.downs = nn.ModuleList()
+        self.ups = nn.ModuleList()
+
+        # Downsampling path
+        for feature in features:
+            self.downs.append(DoubleConv(in_channels, feature))
+            in_channels = feature
+
+        # Bottleneck
+        self.bottleneck = DoubleConv(features[-1], features[-1] * 2)
+
+        # Upsampling path
+        for feature in reversed(features):
+            self.ups.append(nn.ConvTranspose2d(feature * 2, feature, kernel_size=2, stride=2))
+            self.ups.append(DoubleConv(feature * 2, feature))
+
+        self.final_conv = nn.Conv2d(features[0], out_channels, kernel_size=1)
+
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+    def forward(self, x):
+        skip_connections = []
+
+        for down in self.downs:
+            x = down(x)
+            skip_connections.append(x)
+            x = self.pool(x)
+
+        x = self.bottleneck(x)
+        skip_connections = skip_connections[::-1]
+
+        for idx in range(0, len(self.ups), 2):
+            x = self.ups[idx](x)
+            skip_connection = skip_connections[idx // 2]
+
+            if x.shape != skip_connection.shape:
+                x = F.interpolate(x, size=skip_connection.shape[2:])
+
+            x = torch.cat((skip_connection, x), dim=1)
+            x = self.ups[idx + 1](x)
+
+        return self.final_conv(x)
+
