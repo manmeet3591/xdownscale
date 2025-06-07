6eea5005c44e3541b1e44a96e2a8c65fceb69f46
Manmeet Singh <manmeet20singh11@gmail.com>
2025-06-06 20:57:33 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='xdownscale',
     version='0.1.0',
-    description='A PyTorch-based tool to downscale satellite images using SRCNN',
+    description='A PyTorch-based tool to downscale spatiotemporal data',
     author='Your Name',
     packages=find_packages(),
     install_requires=[
